8040153098dbbe1a0edac6322330e33f256745e7
xmamarasulovk <xmamarasulovk@stuba.sk>
2024-01-14 17:28:35 +0500

    Nested if statement implementation.

--- src/main/java/sk/stuba/fei/uim/oop/Game.java
+++ src/main/java/sk/stuba/fei/uim/oop/Game.java
@@ -3,7 +3,7 @@ package sk.stuba.fei.uim.oop;
 public class Game {
     public Game(){
         for(int i=1;i<=100;i++){
-            //mustPrint = 0;
+            /*
             if(i % 3 == 0 && i % 5 == 0)
                 System.out.println("FizzBuzz");
             else if(i % 3 == 0 && i % 5 != 0) {
@@ -13,6 +13,17 @@ public class Game {
                 System.out.println("Buzz");
             else
                 System.out.println(i);
+             */
+            if(i % 3 == 0 || i % 5 == 0){
+                if(i % 3 == 0 && i % 5 == 0)
+                    System.out.println("FizzBuzz");
+                else if(i % 3 == 0)
+                    System.out.println("Fizz");
+                else if(i % 5 == 0)
+                    System.out.println("Buzz");
+                }
+            else
+                System.out.println(i);
+            }
         }
     }
-}
